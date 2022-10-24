83e3cba3fb7094be1b759d9e5a171b0e1d7d85e6
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-10-24 18:34:44 -0500

    updated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Authenticated from '../pages/Authenticated';
+import LogIn from '../pages/LogIn';
 
 export default function Routes({ user }) {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={() => <Authenticated user={user} />} />
+        <Route exact path="/login" component={() => <LogIn />} />
         <Route path="*" component={() => <Authenticated user={user} />} />
       </Switch>
     </div>
